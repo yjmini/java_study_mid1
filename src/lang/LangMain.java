c6f115acd3c4ca18f23898c91af421cf963e55cb
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-06-13 01:09:09 +0900

    JAVA_mid1 lecture source code

--- src/lang/LangMain.java
+++ src/lang/LangMain.java
@@ -3,6 +3,6 @@ package lang;
 public class LangMain {
 
     public static void main(String[] args) {
-        System.out.println("hello java");
+        System.out.println("hello java!");
     }
 }
